111c4dea14e1294704f29e43c52ebe531b2619a7
thao <mrtrados@gmail.com>
2025-03-06 22:29:48 +0900

    Update from HelloWorld to Diffcheck

--- diff-check/src/App.vue
+++ diff-check/src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <DiffCheck msg="Welcome to Your Vue.js App" />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import DiffCheck from "./components/DiffCheck.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    DiffCheck,
   },
 };
 </script>
